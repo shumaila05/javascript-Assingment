0eaede8707193d1eb0cae2f0f0abbcdfd81d1ed6
shumaila05 <shahinaaqibkhan@gmail.com>
2024-06-03 15:16:25 +0500

    ASSINGMENT Chapter 1
    Alerts

---
+++ chapter 1/script.js
@@ -0,0 +1,16 @@
+// Answer 1-----------
+alert ("We're so glad you're here");
+// Answer 2-----------
+alert ("Error! please enter a valid password");
+// Answer 3-----------
+let message = "Welcome to JS land....\n Happy Coding!";
+alert (message);
+// Answer 4----------
+alert ("Welcome to JS Land...");
+
+let message2 = "Happy Coding!\n\u25A0 Prevent this page from creating additional dialogs.";
+alert (message2);
+// Answer 5----------
+alert ("Hello... I can run JS through my web browser's console");
+
+
---
+++ chapter 2/script.js
@@ -0,0 +1,35 @@
+// Answer1-------------
+let username = "shumaila";
+console.log(username);
+// Answer2------------
+let myName = "shumaila sheikh";
+console.log(myName);
+// Answer3-----------
+let message = "Hello World!";
+alert (message);
+// Answer4-----------
+// Student's bio data
+var Name = "John Doe";
+var age = 20;
+var Certified = "Certified for Mobile Application Development";
+
+// Displaying the bio data in alert boxes
+alert("Name: " + Name);
+alert("Age: " + age);
+alert(Certified);
+// Answer5-----------
+let pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+alert (pizza);
+// Answer6----------
+let email = "sheikhshumaila@gmail.com";
+alert ("My Email Address is " + email);
+// Answer7----------
+let book = "I am trying to learn from Book A smarter way to learn JavaScript ";
+alert (book);
+// Answer8----------
+let line = "Yah! I can write HTML content through JavaScript.";
+document.write(line);
+// Answer9-----------
+let style = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+alert (style);
+
